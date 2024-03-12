1a5c01e7ad8cfbc3ec0edb25750171ed94fe9905
Karabo <139490799+KaraboSeekoei@users.noreply.github.com>
2024-03-12 15:10:42 +0200

    Update MyCalculator.java

--- MyCalculator.java
+++ MyCalculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// Karabo Seekoei */
 public class MyCalculator {
     public static void main(String[] args) {
         System.out.println("Welcome to the Simple Calculator!");
